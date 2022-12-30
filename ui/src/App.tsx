7926747968444bff1fc64ad2d0b40dd407765349
Vlad Klokun <vklokun@protonmail.ch>
2022-12-31 01:06:53 +0200

    feat: improve the UI
    This change:
    
    - Replaces the header with a Kubescape logo image.
    - Moves the navigation buttons to the the bottom Paper element.
    - Adds progress indication for deploying Kubescape.
    - Constrains the extension’s main screen with a Container.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,25 @@
 import React, { ChangeEvent, useState, ComponentType } from "react";
 import { createDockerDesktopClient } from "@docker/extension-api-client";
+import { LoadingButton } from "@mui/lab";
 import {
-  Box, Stepper, Step, StepLabel, Button, Grid, Stack, Alert, TextField, Typography,
+  Box, Container, Stepper, Step, StepLabel, Button, Grid, Stack, Alert, Snackbar, TextField, Typography, Paper
 } from "@mui/material";
+import PolicyIcon from "@mui/icons-material/Policy";
+import GppGoodIcon from "@mui/icons-material/GppGood";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 
 import {
   deployKubescapeHelm,
 } from "./helper/kubernetes";
+
+const snackbarDuration = 5000;
+
+const kubescapeSignupURL = "https://cloud.armosec.io/account/sign-up"
+const kubescapeDashboardURL = "https://cloud.armosec.io/dashboard"
+
+const uuidRegex = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/
+const invalidUUIDMessage = "Input is not a valid Account ID."
 
 // Note: This line relies on Docker Desktop's presence as a host application.
 // If you're running this React app in a browser, it won't work properly.
@@ -16,27 +29,43 @@ function useDockerDesktopClient() {
   return client;
 }
 
-const kubescapeSignupURL = "https://cloud.armosec.io/account/sign-up"
-const kubescapeDashboardURL = "https://cloud.armosec.io/dashboard"
-const uuidRegex = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/
+type PageProps = {
+  initial: boolean
+  optional: boolean
+  final: boolean
 
-type ScreenProps = {
   backFn: () => void
   nextFn: () => void
+
+  setBackAvailable: (newValue: boolean) => void
+  setNextAvailable: (newValue: boolean) => void
+
+  onKubescapeDeploy: () => void
+  onComplete: () => void
 }
 
-type PageProps = ScreenProps & {
-  component: ComponentType<ScreenProps>
+type GenericPageProps = PageProps & {
+  component: ComponentType<PageProps>
 }
 
-const Page = ({ component, backFn, nextFn }: PageProps) => {
-  const SpecificPage = component
+const Page = (props: GenericPageProps) => {
+  const SpecificPage = props.component
+
+  // “Back" button is available by default for all pages, except the initial
+  props.setBackAvailable(!props.initial)
+  // "Next” is available by defautl only for the optional pages
+  props.setNextAvailable(props.optional)
+
+  const { component: _, ...specificProps } = props
+
   return (
-    <SpecificPage backFn={backFn} nextFn={nextFn} />
+    <SpecificPage
+      {...specificProps}
+    />
   )
 }
 
-const SignUpScreen = (props: ScreenProps) => {
+const SignUpPage = (props: PageProps) => {
   const openKubescapeSignup = () => {
     client.host.openExternal(kubescapeSignupURL)
   }
@@ -46,25 +75,22 @@ const SignUpScreen = (props: ScreenProps) => {
       <Typography>
         To help you elevate the security of your Kubernetes cluster and provide you with the best insights, we need to connect to your Kubescape Cloud account. If you don’t have one yet, press the “Sign Up” button below and create the account by following the instructions. If you already have an account, feel free to jump to the next step.
       </Typography>
-      <Stack direction="row" justifyContent="space-between" sx={{ mt: 2 }}>
-        <Button disabled variant="text">
-          Back
-        </Button>
+      <Stack direction="row" justifyContent="flex-end" sx={{ mt: 2 }}>
         <Button variant="outlined" onClick={openKubescapeSignup}>
           Sign Up
-        </Button>
-        <Button variant="contained" onClick={props.nextFn}>
-          Next
         </Button>
       </Stack>
     </>
   )
 }
 
-const DeployScreen = (props: ScreenProps) => {
-  const [response, setResponse] = useState<string | undefined>();
+const SecurePage = (props: PageProps) => {
   const [accountID, setAccountID] = useState<string>("");
+
   const [accountIDInvalid, setAccountIDInvalid] = useState<boolean>(true);
+  const [accountIDValidationText, setAccountIDValidationText] = useState<string>("");
+
+  const [deployingKubescape, setDeployingKubescape] = useState<boolean>(false);
   const [kubescapeDeployed, setKubescapeDeployed] = useState<boolean>(false);
 
   const ddClient = useDockerDesktopClient();
@@ -75,92 +101,68 @@ const DeployScreen = (props: ScreenProps) => {
       setAccountIDInvalid(false)
       setAccountID(inputValue)
     } else {
+      setAccountIDValidationText(invalidUUIDMessage)
       setAccountIDInvalid(true)
     }
   }
 
   const handleDeployRequest = async () => {
-    const result = await deployKubescapeHelm(ddClient, accountID);
-    setResponse(result);
+    setDeployingKubescape(true)
+    await deployKubescapeHelm(ddClient, accountID)
+    setDeployingKubescape(false)
+
     setKubescapeDeployed(true)
+
+    props.onKubescapeDeploy()
   }
+
+  props.setNextAvailable(kubescapeDeployed)
 
   return (
     <>
       <Typography>
-        Before Kubescape can help you secure your cluster, please first make sure that the cluster is running. Once your cluster is ready, Kubescape needs your Kubescape Cloud Account ID to deploy. To get the Account ID, log into Kubescape Cloud, click on your Account icon, press the “Copy” button next to your Account ID to copy the account ID, paste it into the input and then deploy Kubescape.
+        Before Kubescape can help you secure your cluster, please first make sure that the cluster is running. Once your cluster is ready, Kubescape needs your Kubescape Cloud Account ID to deploy. To get the Account ID, log into Kubescape Cloud, click on your Account icon, press the “Copy” button next to your Account ID to copy the Account ID, paste it into the input and then deploy Kubescape.
       </Typography>
 
-      {
-        kubescapeDeployed && <Alert></Alert>
-      }
-
-      {kubescapeDeployed ? (
-        <Alert severity="success" sx={{ mt: 2 }}>
-          Kubescape has been successfully deployed!
-        </Alert>
-      ) : (
-        null
-      )}
-
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Stack direction="row" alignItems="start" spacing={2} sx={{ mt: 4 }} justifyContent="space-between">
-            <TextField
-              error={accountIDInvalid}
-              variant="standard"
-              onChange={handleTextChange}
-              label="Your Kubescape account ID"
-              sx={{ width: "50%" }}
-            />
-            <Button
-              disabled={kubescapeDeployed}
-              variant="contained"
-              onClick={handleDeployRequest}
-            >
-              Deploy Kubescape
-            </Button>
-          </Stack>
-        </Grid>
-        <Grid item xs={12}>
+      <Grid container direction="row" justifyContent="space-between" alignItems="bottom" spacing={2}>
+        <Grid item xs={6}>
           <TextField
-            label="Output"
-            sx={{ width: "100%" }}
-            disabled
-            multiline
-            variant="outlined"
-            minRows={5}
-            value={response ?? ""}
+            fullWidth={true}
+            error={accountIDInvalid}
+            variant="standard"
+            onChange={handleTextChange}
+            helperText={accountIDInvalid && accountIDValidationText}
+            label="Your Kubescape Account ID"
           />
         </Grid>
-      </Grid>
-      <Grid item sx={{ mt: 2 }}>
-        <Stack direction="row" justifyContent="space-between">
-          <Button variant="text" onClick={props.backFn}>
-            Back
-          </Button>
-          <Button disabled={!kubescapeDeployed} onClick={props.nextFn}>
-            Next
-          </Button>
-        </Stack>
+        <Grid item>
+          <LoadingButton
+            color={kubescapeDeployed ? "success" : "primary"}
+            loading={deployingKubescape}
+            loadingPosition="end"
+            endIcon={kubescapeDeployed ? <GppGoodIcon /> : <PolicyIcon />}
+            disabled={accountIDInvalid || kubescapeDeployed}
+            variant="contained"
+            onClick={handleDeployRequest}
+          >
+            {kubescapeDeployed ? "Secured" : "Secure with Kubescape"}
+          </LoadingButton>
+        </Grid>
       </Grid>
     </>
   )
 }
 
-const MonitorScreen = (props: ScreenProps) => {
+const MonitorPage = () => {
   const openDashboard = () => {
     client.host.openExternal(kubescapeDashboardURL)
   }
   return (
     <>
       <Typography>
-        Now that Kubescape has been successfully deployed in your cluster, take a look at your Kubescape Cloud dashboard to check how your secure is your cluster and what you can do to improve your security posture.
+        Now that Kubescape has been successfully deployed in your cluster, take a look at your Kubescape Cloud dashboard to check how secure is your cluster and what you can do to improve your security posture.
       </Typography>
-      <Stack direction="row" justifyContent="space-between">
-        <Button variant="text" onClick={props.backFn}>
-          Back
-        </Button>
+      <Stack direction="row" justifyContent="flex-end">
         <Button onClick={openDashboard}>
           Open Dashboard
         </Button>
@@ -172,24 +174,24 @@ const MonitorScreen = (props: ScreenProps) => {
 const steps = [
   {
     label: "Sign Up",
-    optional: false,
-    payload: SignUpScreen,
+    optional: true,
+    payload: SignUpPage,
   },
   {
     label: "Secure",
     optional: false,
-    payload: DeployScreen,
+    payload: SecurePage,
   },
-  {
-    label: "Monitor",
-    optional: false,
-    payload: MonitorScreen,
-  }
 ];
 
 function HorizontalLinearStepper() {
   const [activeStep, setActiveStep] = useState(0);
   const [skipped, setSkipped] = useState(new Set<number>());
+
+  const [showDeployedSnackbar, setShowDeployedSnackbar] = useState<boolean>(false);
+
+  const [backAvailable, setBackAvailable] = useState<boolean>(false);
+  const [nextAvailable, setNextAvailable] = useState<boolean>(false);
 
   const isStepOptional = (step: number) => getStep(step).optional;
 
@@ -229,21 +231,18 @@ function HorizontalLinearStepper() {
     setActiveStep(0);
   };
 
-  const getStep = (step: number) => steps[step]
+  const getStep = (step: number) => steps[step];
+
+  const flowCompleted = (step: number) => step === steps.length;
 
   return (
-    <Box sx={{ width: '100%' }}>
+    <>
       <Stepper activeStep={activeStep} sx={{ mt: "1em" }}>
         {steps.map((step, index) => {
           const stepProps: { completed?: boolean } = {};
           const labelProps: {
             optional?: React.ReactNode;
           } = {};
-          if (isStepOptional(index)) {
-            labelProps.optional = (
-              <Typography variant="caption">Optional</Typography>
-            );
-          }
           if (isStepSkipped(index)) {
             stepProps.completed = false;
           }
@@ -254,37 +253,87 @@ function HorizontalLinearStepper() {
           );
         })}
       </Stepper>
-      {activeStep === steps.length ? (
-        <>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
+
+      {flowCompleted(activeStep) ? (
+        <Box>
+          <Typography variant="h3" sx={{ mt: 3, mb: 1 }}>
+            Kubescape is securing your cluster
           </Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Box sx={{ flex: '1 1 auto' }} />
-            <Button onClick={handleReset}>Reset</Button>
-          </Box>
-        </>
+          <MonitorPage />
+        </Box>
       ) : (
-        <>
-          <Typography variant="h3" sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}: {getStep(activeStep).label}</Typography>
-          <Page component={getStep(activeStep).payload} backFn={handleBack} nextFn={handleNext} />
-        </>
+        <Box>
+          <Typography variant="h3" sx={{ mt: 3, mb: 1 }}>Step {activeStep + 1}: {getStep(activeStep).label}</Typography>
+          <Page
+            component={getStep(activeStep).payload}
+            optional={getStep(activeStep).optional}
+            initial={activeStep === 0}
+            final={activeStep === (steps.length - 1)}
+            backFn={handleBack}
+            nextFn={handleNext}
+            onKubescapeDeploy={() => setShowDeployedSnackbar(true)}
+            onComplete={() => setNextAvailable(true)}
+            setBackAvailable={setBackAvailable}
+            setNextAvailable={setNextAvailable}
+          />
+
+          <Snackbar
+            open={showDeployedSnackbar}
+            autoHideDuration={snackbarDuration}
+            anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            onClose={() => setShowDeployedSnackbar(false)}
+          >
+            <Alert severity="success" sx={{ mt: 2 }}>
+              Kubescape has been successfully deployed!
+            </Alert>
+          </Snackbar>
+
+          <Paper sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }} elevation={3}>
+            <Container maxWidth="md">
+              <Stack direction="row" justifyContent="space-between">
+                <Button
+                  disabled={!backAvailable}
+                  variant="text"
+                  startIcon={<KeyboardArrowLeft />}
+                  color="inherit"
+                  onClick={handleBack}
+                >
+                  Back
+                </Button>
+                <Button
+                  disabled={!nextAvailable}
+                  variant="text"
+                  endIcon={<KeyboardArrowRight />}
+                  onClick={handleNext}
+                >
+                  Next
+                </Button>
+              </Stack>
+            </Container>
+          </Paper>
+        </Box>
       )}
-    </Box>
+    </>
   );
 }
 
 export function App() {
   return (
-    <>
-      <Typography variant="h3">Kubescape</Typography>
-      <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
-        Kubescape is a service that aims to secure your Kubernetes clusters by
-        scanning it for misconfigurations and vulnerabilities and offering
-        remediation where possible.
-      </Typography>
+    <Container maxWidth="md">
+      <Grid container justifyContent="center">
+        <Grid item>
+          <Box>
+            <Box
+              component="img"
+              src="images/kubescape-stacked-color.svg"
+              height="12.5em"
+            />
+          </Box>
+        </Grid>
+      </Grid>
 
       <HorizontalLinearStepper />
-    </>
+
+    </Container>
   );
 }
